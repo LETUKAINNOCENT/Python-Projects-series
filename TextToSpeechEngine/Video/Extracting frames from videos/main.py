b4b37cc396de73a5df1c9b37cf2ad53601904f22
Letuka Letuka <letukainnocent6@gmail.com>
2026-03-24 12:10:05 +0000

    Add image processing and video frame extraction features
    - Implemented OCR functionality using Pytesseract in main.py for text extraction from images.
    - Created a new script for extracting frames from videos, allowing users to save frames at specified intervals.
    - Added sample video files for testing frame extraction functionality.

---
+++ TextToSpeechEngine/Video/Extracting frames from videos/main.py
@@ -0,0 +1,35 @@
+import os
+import cv2
+from glob import glob
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def save_frame(video_path, save_dir, gap=1):  # default = 1 (max frames)
+    name = os.path.splitext(os.path.basename(video_path))[0]
+    save_path = os.path.join(save_dir, name)
+    create_dir(save_path)
+
+    cap = cv2.VideoCapture(video_path)
+    idx = 0
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Save every frame when gap=1
+        if idx % gap == 0:
+            cv2.imwrite(os.path.join(save_path, f"{idx}.png"), frame)
+
+        idx += 1
+
+    cap.release()
+
+if __name__ == "__main__":
+    video_paths = glob("videos/*")
+    save_dir = "__local__save"
+
+    for path in video_paths:
+        save_frame(path, save_dir)  # no need to pass gap
